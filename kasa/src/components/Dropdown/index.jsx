44826453ca032708badc189f4a0c52fd0f8c7314
roy <roy.framery@gmail.com>
2022-02-10 15:41:21 +0100

    add drowdown componen About us page is donet

--- kasa/src/components/Dropdown/index.jsx
+++ kasa/src/components/Dropdown/index.jsx
@@ -1,30 +1,30 @@
-import aboutUs from '../../__mocks__/aboutUs'
-
 // CSS
 import './Dropdown.css'
 
-const Dropdown = ({ title }) => {
-    const content = (title) => {
-        switch (title) {
-            case 'fiabilité':
-                return aboutUs.fiability
-                break
-            case 'respect':
-                return aboutUs.respect
-                break
-            case 'service':
-                return aboutUs.service
-                break
-            case 'sécurité':
-                break
-            default:
+const Dropdown = ({ title, content }) => {
+    const handleDropdown = (e) => {
+        const contentNode = e.target.nextSibling
+        console.log(contentNode)
+        const arrow = e.target.lastChild
+
+        if (contentNode.classList.contains('show')) {
+            contentNode.classList.remove('show')
+            arrow.classList.remove('rotate')
+        } else {
+            contentNode.classList.add('show')
+            arrow.classList.add('rotate')
         }
     }
 
     return (
         <div className="dropdown">
-            <div className="dropdown__title">{title}</div>
-            <div className="dropdown__content">{content}</div>
+            <div className="dropdown__button" onClick={handleDropdown}>
+                <div className="dropdown__title">{title}</div>
+                <img className="drowdown__arrow" src="/arrow-down.svg" alt="" />
+            </div>
+            <div className="dropdown__content">
+                <p className="dropdown__text">{content}</p>
+            </div>
         </div>
     )
 }
--- kasa/src/pages/AboutUs/index.jsx
+++ kasa/src/pages/AboutUs/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import Banner from '../../components/Banner'
-// import aboutUs from '../../__mocks__/aboutUs'
+import aboutUs from '../../__mocks__/aboutUs'
+import Dropdown from '../../components/Dropdown'
 
 // CSS
 import './AboutUs.css'
@@ -10,6 +11,26 @@ const AboutUs = () => {
         document.title = 'À propos'
     }, [])
 
+    const content = (title) => {
+        switch (title) {
+            case 'fiabilité':
+                return aboutUs.fiability
+                break
+            case 'respect':
+                return aboutUs.respect
+                break
+            case 'service':
+                return aboutUs.service
+                break
+            case 'sécurité':
+                return aboutUs.security
+                break
+            default:
+                console.log('default')
+                break
+        }
+    }
+
     const image =
         window.screen.width <= '576'
             ? '/bannerAboutUsSmall.png'
@@ -17,7 +38,12 @@ const AboutUs = () => {
     return (
         <div className="aboutUs">
             <Banner image={image} />
-            <div className="aboutUs__dropdowns">{/* dropdowns */}</div>
+            <div className="aboutUs__dropdowns">
+                <Dropdown title="fiabilité" content={content('fiabilité')} />
+                <Dropdown title="respect" content={content('respect')} />
+                <Dropdown title="service" content={content('service')} />
+                <Dropdown title="sécurité" content={content('sécurité')} />
+            </div>
         </div>
     )
 }
